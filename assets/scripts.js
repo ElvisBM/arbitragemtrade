78a7584e8f630ec5510850dc54b61ea1bf007837
Elvis B. Martins <elvisbmartins@gmail.com>
2019-06-14 15:58:50 -0300

    calculadoras de passiva porcetagens 3 e 5 - teste

---
+++ assets/scripts.js
@@ -0,0 +1,69 @@
+/**
+ * scripts.js
+ */
+(function($) {
+	$( document ).ready(function() {
+
+
+
+        $("input").on('keyup keypress blur change paste', function() {
+           calc();
+        });
+
+        // $("input").keypress(function() {
+        //     calc();
+        //  });
+
+        $("#aumento").keypress(function() {
+            calc();
+         });
+
+        function calc(){
+            var tem = formatVal( $('#tembtc').val() );
+            var nc = formatVal( $('#nc').val()) ;
+            var aumento = formatVal( $('#aumento').val() );
+
+
+            if( nc > 0 && nc !== null ) {
+                var passiva = nc + aumento;
+                // var passivaResul = passiva.toString().replace(".",",");
+                // var passivaResul = passivaResul.slice(0, 2) + "." + passivaResul.slice(2, 8);
+                $('#passiva').html( formatSaida( passiva ) );
+
+                var aumentoPorcetagem = ( ( passiva - tem ) / tem ) * 100;
+                var spread = aumentoPorcetagem.toFixed(2);
+                $('#spread').html(spread + "%");
+            }
+        }
+
+        $("#valorporcetagem").on('keyup keypress blur change paste', function() {
+           var valor = formatVal( $(this).val() );
+           
+           var porc03 = valor * (0.3/100); 
+           var result03 = valor - porc03;
+
+           var porc05 = valor * (0.5/100); 
+           var result05 =  valor - porc05;
+
+           if( valor > 0 && valor !== null ) {
+            $('#porc03').html( formatSaida( result03 ) );
+            $('#porc05').html( formatSaida( result05 ) );
+           }
+         
+        });
+ 
+
+        function formatVal( valor ){
+            var retorno = valor.replace(".", "");
+            var retorno =  retorno.replace(",",".");
+            var retorno =  retorno.replace("R$","");
+            return parseFloat( retorno , 10);
+        }
+
+        function formatSaida ( valor ){
+            var retorno = valor.toString().replace(".",",");
+            var retorno = retorno.slice(0, 2) + "." + retorno.slice(2, 8);
+            return retorno;
+        }
+	});
+})(jQuery);
